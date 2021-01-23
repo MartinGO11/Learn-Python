0693956f871c6f523b473034ec44c22e494a1233
MartinGO11 <ogutierrez.m94@gmail.com>
2021-01-23 00:36:15 +0000

    not finish

--- exercises/05-User-Inputed-Values/app.py
+++ exercises/05-User-Inputed-Values/app.py
@@ -1,3 +1,3 @@
-age = int(input('What is your age?\n'))
+age = int(input('What is your age?\10'))
 # CHANGE THE CODE BELOW TO ADD 10 TO AGE
-print("Your age is: "+str(age))
+print("Your age is: "+str(age+10))
